3d6e11d12542f514581d7386a8ad06f841270f8f
Andrey Raygorodskiy <andrey.raygorodskiy@gmail.com>
2014-08-12 20:12:35 +0400

    implement random folder decaration

--- photo_gallary.py
+++ photo_gallary.py
@@ -2,13 +2,12 @@ import json
 import mimetypes
 import os
 import random
-from flask import Flask, render_template, request, Response
+from flask import Flask, render_template, request, Response, abort
 from flask.helpers import make_response
 from flask.ext.cache import Cache
 import zipstream
-import zipfile
 
-# from PIL import Image
+from PIL import Image
 # im = Image.open(image_filename)
 # width, height = im.size
 
@@ -16,10 +15,44 @@ S = '983db650f7f79bc8e87d9a3ba418aefc'
 
 app = Flask(__name__)
 cache = Cache(app, config={'CACHE_TYPE': 'simple'})
-ROOT_DIR = 'y:\\'
+ROOT_DIR = 'E:\\'
 FILE_EXTS = ('.JPG')
 # from werkzeug.contrib.cache import SimpleCache
 # cache = SimpleCache()
+
+@app.route('/css/css.css')
+# @cache.cached(timeout=1*60*60)
+def css():
+    def generate():
+        from random import choice as rand
+
+        classs = range(11)
+        pxx = range(-10, 11, 2)
+        degs = range(-10, 11, 2)
+        for item in classs:
+            deg = rand(degs)
+            yield '.folder_%s:before {\n' % item
+            yield '    top: %spx;\n' % rand(pxx)
+            yield '    left: %spx;\n' % rand(pxx)
+            yield '    -webkit-transform: rotate(%sdeg);\n' % deg
+            yield '    -moz-transform: rotate(%sdeg);\n' % deg
+            yield '    -o-transform: rotate(%sdeg);\n' % deg
+            yield '    -ms-transform: rotate(%sdeg);\n' % deg
+            yield '    transform: rotate(%sdeg);\n' % deg
+            yield '}\n'
+            deg = rand(degs)
+            yield '.folder_%s:after {\n' % item
+            yield '    top: %spx;\n' % rand(pxx)
+            yield '    left: %spx;\n' % rand(pxx)
+            yield '    -webkit-transform: rotate(%sdeg);\n' % deg
+            yield '    -moz-transform: rotate(%sdeg);\n' % deg
+            yield '    -o-transform: rotate(%sdeg);\n' % deg
+            yield '    -ms-transform: rotate(%sdeg);\n' % deg
+            yield '    transform: rotate(%sdeg);\n' % deg
+            yield '}\n'
+
+    return Response(generate(), mimetype='text/css')
+
 
 @app.route('/export.zip', methods=['POST'])
 def export():
@@ -38,14 +71,19 @@ def export():
     return response
 
 
-@app.route('/folder_icon/<folder>/folder.jpg')
+@app.route('/folder_icon/<path:folder>/folder.jpg')
+# @cache.cached(timeout=1*60*60)
 def folder_icon(folder):
     dirpath = os.path.join(ROOT_DIR, folder, S)
-    if not os.path.exists(dirpath):
-        return ''
-    filenames = os.listdir(dirpath)
-    f_name = random.choice(filenames)
-    file = open(os.path.join(dirpath, f_name), 'rb')
+    f_name = None
+    if os.path.exists(dirpath):
+        filenames = os.listdir(dirpath)
+        f_name = random.choice(filenames)
+    if f_name == None:
+        f_name = 'no-image.jpg'
+        dirpath = os.path.dirname(__file__)
+    path_join = os.path.join(dirpath, f_name)
+    file = open(path_join, 'rb')
     file_read = file.read()
     file.close()
     response = make_response(file_read)
@@ -55,15 +93,19 @@ def folder_icon(folder):
 
 
 @app.route('/th/<path:path>')
+@cache.cached(timeout=1 * 60 * 60)
 def get_file_thumbnail(path):
     real_path = os.path.join(ROOT_DIR, path)
-
-    # dir_path = os.path.dirname(real_path)
-
     path_rindex = real_path.rindex('/')
     th_real_path = real_path[:path_rindex] + '/' + S + real_path[path_rindex:]
     if not os.path.exists(th_real_path):
-        return make_response(None, 404)
+        image_open = Image.open(real_path)
+        image_open.thumbnail((280, 280), resample=Image.ANTIALIAS)
+        path_dirname = os.path.dirname(th_real_path)
+        if not os.path.exists(path_dirname):
+            os.mkdir(path_dirname)
+        image_open.save(th_real_path)
+        # th_real_path = real_path
     file = open(th_real_path, 'rb')
     file_read = file.read()
     file.close()
@@ -87,7 +129,7 @@ def create_breadcrumb(path):
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 # @cache.cached(timeout=1*60*60)
-def get_folder(path):
+def get_folder_or_file(path):
     real_path = os.path.join(ROOT_DIR, path)
     if os.path.isdir(real_path):
         breadcrumb = create_breadcrumb(path)
@@ -122,7 +164,7 @@ def get_folder(path):
 # @app.route('/files/<folder_name>/<file_name>')
 # def get_file_content(folder_name, file_name):
 # dirpath = os.path.join(ROOT_DIR, folder_name)
-#     file = open(os.path.join(dirpath, file_name), 'rb')
+# file = open(os.path.join(dirpath, file_name), 'rb')
 #     file_read = file.read()
 #     file.close()
 #     response = make_response(file_read)
@@ -130,7 +172,6 @@ def get_folder(path):
 #     response.headers['Content-Type'] = mimetypes.guess_type(file_name)[0]
 #     response.headers['Content-Length'] = os.path.getsize(file.name)
 #     return response
-
 
 
 
