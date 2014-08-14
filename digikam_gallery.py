539f6ef46a12d227dd70ca6f56af0c760e7dcc8e
Andrey Raygorodskiy <andrey.raygorodskiy@gmail.com>
2014-08-14 18:13:42 +0400

    rewrite almost all

---
+++ digikam_gallery.py
@@ -0,0 +1,161 @@
+# -*- coding: utf-8 -*-
+from StringIO import StringIO
+
+__author__ = 'araigorodskiy'
+import json
+import mimetypes
+import os
+import random
+from flask import Flask, render_template, request, Response, abort, send_file
+from flask.helpers import make_response
+from flask.ext.cache import Cache
+import zipstream
+from flask.ext.sqlalchemy import SQLAlchemy
+
+from PIL import Image
+
+app = Flask(__name__)
+ROOT_DIR = u'/home/araigorodskiy/Изображения'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///%s/digikam4.db' % ROOT_DIR
+app.config['SQLALCHEMY_ECHO'] = True
+
+db = SQLAlchemy(app)
+cache = Cache(app, config={'CACHE_TYPE': 'simple'})
+
+from model import *
+
+
+@app.route('/css/css.css')
+# @cache.cached(timeout=1*60*60)
+def css():
+    def generate():
+        from random import choice as rand
+
+        classs = range(11)
+        pxx = range(-10, 11, 2)
+        degs = range(-10, 11, 2)
+        for item in classs:
+            deg = rand(degs)
+            yield '.folder_%s:before {\n' % item
+            yield '    top: %spx;\n' % rand(pxx)
+            yield '    left: %spx;\n' % rand(pxx)
+            yield '    -webkit-transform: rotate(%sdeg);\n' % deg
+            yield '    -moz-transform: rotate(%sdeg);\n' % deg
+            yield '    -o-transform: rotate(%sdeg);\n' % deg
+            yield '    -ms-transform: rotate(%sdeg);\n' % deg
+            yield '    transform: rotate(%sdeg);\n' % deg
+            yield '}\n'
+            deg = rand(degs)
+            yield '.folder_%s:after {\n' % item
+            yield '    top: %spx;\n' % rand(pxx)
+            yield '    left: %spx;\n' % rand(pxx)
+            yield '    -webkit-transform: rotate(%sdeg);\n' % deg
+            yield '    -moz-transform: rotate(%sdeg);\n' % deg
+            yield '    -o-transform: rotate(%sdeg);\n' % deg
+            yield '    -ms-transform: rotate(%sdeg);\n' % deg
+            yield '    transform: rotate(%sdeg);\n' % deg
+            yield '}\n'
+
+    return Response(generate(), mimetype='text/css')
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/<t>/<int:tag_id>/')
+def peoples_files(t, tag_id):
+    q = []
+    if t == 'Albums':
+        q = Images.query.filter(Images.album_id == tag_id)
+    elif t == 'Peoples':
+        q = Images.query.join(ImageTags, ImageTags.imageid == Images.id).filter(ImageTags.tagid == tag_id)
+    elif t == 'Best':
+        if tag_id == 0: tag_id = -1
+        q = Images.query.join(ImageInformation, ImageInformation.imageid == Images.id).filter(
+            ImageInformation.rating == tag_id).filter(Images.status == 1)
+    else:
+        abort(400)
+    return render_template('album.html', images=q)
+
+
+@app.route('/<t>/')
+def albums(t):
+    query = []
+    if t == 'Albums':
+        query = Albums.query.filter(Albums.albumRoot_id > 0)
+    elif t == 'Peoples':
+        query = Tags.query.join(TagProperties, TagProperties.tagid == Tags.id) \
+            .filter(TagProperties.property == 'kfaceId')
+    elif t == 'Best':
+        query = [
+            Ratings(0, u'Без оценки'),
+            Ratings(1, u'1'),
+            Ratings(2, u'2'),
+            Ratings(3, u'3'),
+            Ratings(4, u'4'),
+            Ratings(5, u'5'),
+        ]
+    else:
+        abort(400)
+
+    return render_template('albums.html',
+                           rand=random.choice,
+                           albums=query)
+
+
+@app.route('/f/<type>/<album_id>/<image_id>/<image_name>')
+@cache.cached(timeout=1 * 60 * 60)
+def get_image(type, album_id, image_id, image_name):
+    image = Images.query.get_or_404(image_id)
+    path = image.get_path()
+    real_path = ROOT_DIR + path
+    file_io = open(real_path, 'rb')
+    file_read = file_io.read()
+    file_io.close()
+    response = make_response(file_read)
+    response.headers['Cache-Control'] = 'private, max-age=%d' % (60 * 60 * 24)
+    response.headers['Content-Type'] = mimetypes.guess_type(real_path)[0]
+    response.headers['Content-Length'] = os.path.getsize(file_io.name)
+    return response
+
+
+@app.route('/th/<album_id>/<image_id>/<image_name>')
+# @cache.cached(timeout=1 * 60 * 60)
+def get_thumb(album_id, image_id, image_name):
+    image = Images.query.get_or_404(image_id)
+    path = image.get_path()
+    real_path = ROOT_DIR + path
+    image_open = Image.open(real_path)
+    image_open.thumbnail((280, 280), resample=Image.ANTIALIAS)
+    img_io = StringIO()
+    image_open.save(img_io, 'JPEG', quality=70)
+    img_io.seek(0)
+    return send_file(img_io, mimetype='image/jpeg')
+    # response = make_response(file_read)
+    # response.headers['Cache-Control'] = 'private, max-age=%d' % (60 * 60 * 24)
+    # response.headers['Content-Type'] = mimetypes.guess_type(real_path)[0]
+    # response.headers['Content-Length'] = os.path.getsize(file_io.name)
+    # return response
+
+
+@app.route('/<t>/<album_id>/folder_icon.jpg')
+def folder_icon(t, album_id):
+    album = None
+    if t == 'Albums':
+        album = Albums.query.get(album_id)
+    elif t == 'Peoples':
+        album = Tags.query.get(album_id)
+    elif t == 'Best':
+        album = Tags.query.get(album_id)
+    else:
+        abort(404)
+    if album is None or album.icon is None:
+        return send_file(open(os.path.join(os.path.dirname(__file__), 'no-image.jpg')))
+    return get_thumb(None, album.icon.id, album.icon.name)
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='0.0.0.0')
---
+++ model.py
@@ -0,0 +1,82 @@
+from digikam_gallery import db
+
+__author__ = 'araigorodskiy'
+
+
+class Albums(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    # albumRoot = db.relationship("Albums", foreign_keys=[albumRoot_id])
+    relativePath = db.Column(db.Text())
+    icon_id = db.Column('icon', db.Integer, db.ForeignKey('images.id'))
+
+    def get_link(self):
+        return '%d/' % self.id
+
+    def get_name(self):
+        return self.relativePath[1:]
+
+
+Albums.albumRoot_id = db.Column('albumRoot', db.Integer, db.ForeignKey(Albums.id))
+Albums.albumRoot = db.relationship("Albums", backref="children", remote_side=Albums.id)
+
+
+class Images(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Text())
+    status = db.Column(db.Integer)
+    album_id = db.Column('album', db.Integer, db.ForeignKey('albums.id'))
+    album = db.relationship("Albums", backref="images", remote_side=Albums.id, foreign_keys=[album_id])
+
+    def get_path(self):
+        return self.album.albumRoot.relativePath + self.album.relativePath + '/' + self.name
+
+    def get_path_with_ids(self):
+        return '%d/%d/%s' % (self.album.id, self.id, self.name)
+
+
+Albums.icon = db.relationship("Images", backref="images", remote_side=Images.id, foreign_keys=[Albums.icon_id])
+
+
+class Tags(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Text())
+    pid = db.Column(db.Integer)
+    icon_id = db.Column('icon', db.Integer, db.ForeignKey('images.id'))
+    icon = db.relationship("Images", backref="tags", remote_side=Images.id, foreign_keys=[icon_id])
+
+    def get_link(self):
+        return '%d/' % self.id
+
+    def get_name(self):
+        return self.name
+
+
+class TagProperties(db.Model):
+    __tablename__ = 'TagProperties'
+    tagid = db.Column(db.Integer, db.ForeignKey('tags.id'), primary_key=True)
+    property = db.Column(db.Text(), primary_key=True)
+    value = db.Column(db.Text())
+
+
+class ImageTags(db.Model):
+    __tablename__ = 'ImageTags'
+    imageid = db.Column(db.Integer, db.ForeignKey('images.id'), primary_key=True)
+    tagid = db.Column(db.Integer, db.ForeignKey('tags.id'), primary_key=True)
+
+
+class ImageInformation(db.Model):
+    __tablename__ = 'ImageInformation'
+    imageid = db.Column(db.Integer, db.ForeignKey('images.id'), primary_key=True)
+    rating = db.Column(db.Integer)
+
+
+class Ratings():
+    def __init__(self, rating, name):
+        self.rating = rating
+        self.name = name
+
+    def get_link(self):
+        return '%d/' % self.rating
+
+    def get_name(self):
+        return self.name
